241ff945248c4268bf980cada4bfa1f1637305fd
ionstengaci <stengaciion.g@gmail.com>
2023-04-26 21:14:48 -0500

    some work on js and css

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // Your code here
+
+
+// const table = function(makeRow)
+
+const table = document.getElementsByTagName("table")[0]
+
+function makeRow (){
+    const row = document.createElement('tr')
+    for ( let i = 0 ; i < 15 ; i++ ){
+    const td = document.createElement('td')
+    row.appendChild(td)
+}
+    table.appendChild(row)
+}
+makeRow();
+makeRow();
